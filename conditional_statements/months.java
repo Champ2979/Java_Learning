9bd4ba62719cef676a92df48f8b6fb0b1ac0c530
Pranav Pratyush <99145117+Champ2979@users.noreply.github.com>
2023-06-14 01:48:34 +0530

    Update months.java

--- conditional_statements/months.java
+++ conditional_statements/months.java
@@ -1,7 +1,9 @@
 
-//Ask the user to enter the number of the month & print the name
-//of the month. For eg - For ‘1’ print ‘January’, ‘2’ print ‘February’ &
-//so on.
+/*
+Ask the user to enter the number of the month & print the name
+of the month. For eg - For ‘1’ print ‘January’, ‘2’ print ‘February’ &
+so on.
+*/
 
 package conditional_statements;
 
