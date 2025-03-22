71f8650621883b3168f80ef70649fc9820fef30b
Pranav Pratyush <pranavpratyush2007@gmail.com>
2025-03-22 22:36:29 +0530

    some changes are there

--- AddThreeNumbers.java
+++ AddThreeNumbers.java
@@ -1,14 +1,22 @@
-
+import java.util.*;
 // online compiler
 class AddThreeNumbers {
     public static void main(String[] args){
-         System.out.print("The sum of these three numbers is: ");
-         int num1 = 45;
-         int num2 = 36;
-         int num3 = 89;
-         int sum = num1 + num2+ num3;
-        System.out.print(sum);
-        
+         Scanner s = new Scanner(System.in);
+         System.out.print("Enter the number a: ");
+         int a = s.nextInt();
+
+         System.out.print("Enter the number b: ");
+         int b = s.nextInt();
+
+         System.out.print("Enter the number c: ");
+         int c = s.nextInt();
+
+         int ans = (a+b+c);
+
+         System.out.println("The sum of the three numbers are: "+ans);
+
+         s.close();
         
     }
 }
